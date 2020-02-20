17ed4a8de9e0210f9632f274b03809cf1be7ea24
fatimahyderi <fatimakhurram84@gmail.com>
2020-02-20 12:13:53 +0500

    bootstrap

--- website/website/urls.py
+++ website/website/urls.py
@@ -18,4 +18,5 @@ from django.contrib import admin
 from django.urls import include, path
 urlpatterns = [
     path('students/', include('students.urls')),
+    path('', include('bootstrap.urls')),
     path('admin/', admin.site.urls),]
